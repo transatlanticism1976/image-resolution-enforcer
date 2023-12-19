b84d86fe64a2b3f8ee6a2f0b3fd54ad9a7e80358
engresmods <146706527+engresmods@users.noreply.github.com>
2023-12-18 19:15:22 -0500

    pivot from stream.submissions

--- script.py
+++ script.py
@@ -8,24 +8,26 @@ CLIENT_ID = ""
 CLIENT_SECRET = ""
 USER_AGENT = "script:ImageResolutionEnforcer:v0.0.1 (by /u/)"
 
-MIN_WIDTH = 5100 # pixels
+MIN_WIDTH = 5100 # pixels. equivalent to 600DPI for a standard letter size paper
 
 def process_submissions(reddit):
-    for submission in reddit.subreddit('EngineeringResumes').stream.submissions():
+    subreddit = reddit.subreddit('EngineeringResumes')
+    for submission in subreddit.new(limit=10):
         if submission.link_flair_text in ('Question','Meta','Success Story!'):
             return
         if submission.is_self:
-            width = get_width(submission) # unit: pixels
-            resolution = round(width/8.5) # convert pixels to "DPI"
+            width = get_width(submission)
+            resolution = round(width/8.5) # convert to DPI
             if width < MIN_WIDTH:
-                print(f"REJECT {resolution}DPI {submission.author}")
+                print(f"{submission.created_utc} REJECT {resolution}DPI {submission.author}")
                 # submission.report(f"low-res image detected: {resolution}DPI")
                 return
             else:
-                print(f"PASS {resolution}DPI {submission.author}")
+                print(f"{submission.created_utc} PASS {resolution}DPI {submission.author}")
                 return
 
 def get_width(submission):
+    print('D')
     s = submission.selftext
     keyword = 'width=' # extract image width after this string in image URL
     try:
@@ -33,18 +35,18 @@ def get_width(submission):
     except ValueError:
         # print(ValueError) # TODO test
         return
-    end = start + len(keyword) + 4 # extract 4 digits
     start = start + len(keyword)
-    if s[end-1] == '&': # if image width is only 3 digits
+    end = start + 4 # extract 4 digits
+    if s[end-1] == '&': # if width is only 3 digits
         return int(s[start:(end-1)])
     else:
         return int(s[start:end])
 
-#TODO create a main() function
 if __name__ == "__main__":
     process_submissions(praw.Reddit(
         username=USERNAME, 
         password=PASSWORD, 
         client_id=CLIENT_ID, 
         client_secret=CLIENT_SECRET, 
-        user_agent=USER_AGENT))
+        user_agent=USER_AGENT
+    ))
