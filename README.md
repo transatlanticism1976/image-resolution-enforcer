8821588b2c33f18af89434624347c2e24891fb87
engresmods <146706527+engresmods@users.noreply.github.com>
2023-12-19 15:31:39 -0500

    tidy up

---
+++ README.md
@@ -0,0 +1,2 @@
+# image-resolution-enforcer
+bot for r/EngineeringResumes to detect/report/remove low-resolution submissions
--- script.py
+++ script.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 import praw
-
 from datetime import datetime
 
 USERNAME = ""
@@ -10,30 +9,34 @@ CLIENT_ID = ""
 CLIENT_SECRET = ""
 USER_AGENT = "script:ImageResolutionEnforcer:v0.0.1 (by /u/)"
 
-MIN_WIDTH = 5100 # pixels. equivalent to 600DPI for a standard letter size paper
+MIN_WIDTH = 5100 # width in pixels. equivalent to 600DPI for a standard letter size paper
 
 def process_submissions(reddit):
     subreddit = reddit.subreddit('EngineeringResumes')
     for submission in subreddit.new(limit=25):
         timestamp = datetime.utcfromtimestamp(int(submission.created_utc))
         if submission.link_flair_text in ('Question','Meta','Success Story!'):
+            print(f"{timestamp} {submission.link_flair_text} {submission.author}")
             continue   
         if submission.is_self:  
             width = get_width(submission)
             if width == 0:
+                print(f"{timestamp} INVALID {submission.author} {submission.link_flair_text}")
                 continue
             resolution = round(width/8.5) # convert to DPI
             if width < MIN_WIDTH:
-                print(f"{timestamp} REJECT {resolution}DPI {submission.author}")
-                submission.report(f"low-resolution image detected: {resolution}DPI")
+                print(f"{timestamp} REJECT {resolution}DPI {submission.author} {submission.link_flair_text}")
+                # reddit.redditor("").message(subject="blurry image alert", message=f"{submission.permalink} {submission.link_flair_text} {resolution}DPI")
+                # submission.report(f"low-res image detected: {resolution}DPI")
                 return
             else:
-                print(f"{timestamp} PASS {resolution}DPI {submission.author}")
+                print(f"{timestamp} PASS {submission.link_flair_text} {submission.author}")
                 return
+
 
 def get_width(submission):
     s = submission.selftext
-    keyword = 'width=' # extract image width after this string in image URL
+    keyword = 'width=' # keyword found in image URL: '...width=5100...'
     try:
         start = s.index(keyword)
     except ValueError: # keyword not found
