63dab7ef67c6e3ecdb9d4903f40af602339a8599
engresmods <146706527+engresmods@users.noreply.github.com>
2023-12-22 11:20:43 -0500

    update script.py

--- script.py
+++ script.py
@@ -2,25 +2,14 @@
 
 import praw
 import os
-import time
+
 from datetime import datetime
 from dotenv import load_dotenv
 
-MIN_WIDTH = 5100  # image width in pixels, equivalent to 600DPI for a standard letter size paper
-LIMIT = 50
-counter = 0
+MIN_IMAGE_WIDTH = 5100  # image width in pixels, equivalent to 600DPI for a standard letter size paper
+LIMIT = 10
 
-
-def main():
-    start_time = time.time()
-
-    reddit = instantiate_reddit()
-
-    process_submissions(reddit)
-
-    print("completed processing submissions")
-    print(f"total number of submissions processed: {counter}")
-    print("program took", time.time() - start_time, "seconds to run")
+num_reports = 0
 
 
 def instantiate_reddit():
@@ -31,7 +20,6 @@ def instantiate_reddit():
     client_secret = os.getenv("CLIENT_SECRET")
     user_agent = "script:ImageResolutionEnforcer:v0.0.1 (by /u/" + username + ")"
 
-    print("authenticating..")
     reddit = praw.Reddit(
         username=username,
         password=password,
@@ -46,44 +34,48 @@ def instantiate_reddit():
 
 
 def process_submissions(reddit):
-    print("processing submissions...")
     subreddit = reddit.subreddit("EngineeringResumes")
     for submission in subreddit.new(limit=LIMIT):
-        global counter
-        counter += 1
+        global num_reports
         timestamp = datetime.fromtimestamp(int(submission.created_utc))
         if submission.link_flair_text in ("Meta", "Success Story!"):
             print(
                 f"{timestamp} IGNORE {submission.author} {submission.link_flair_text}"
             )
+            num_reports += 1
 
         elif submission.link_flair_text == ("Question"):
             if any(x in submission.selftext for x in ["jpg", "jpeg", "png", "imgur"]):
                 print(
-                    f"{timestamp} QUESTION: IMAGE DETECTED {submission.author}"
+                    f"{timestamp} QUESTION w/ IMAGE {submission.author} {submission.title}"
                 )
                 submission.report(
                     f"potential incorrect usage of 'Question' flair. change to more appropriate flair if necessary"
                 )
+                num_reports += 1
             elif submission.selftext == "": # no body text
-                print(f"{timestamp} QUESTION: NO BODY TEXT {submission.author}")
+                print(f"{timestamp} QUESTION w/o BODY TEXT {submission.author} {submission.title}")
                 submission.report(
                     f"potential incorrect usage of 'Question' flair. change to more appropriate flair if necessary"
                 )
+                num_reports += 1
             else:
-                print(f"{timestamp} QUESTION: NO BODY TEXT {submission.author}")
+                print(f"{timestamp} QUESTION PASS {submission.author}")
 
         else:
-            width = get_image_width(submission)
-            if width == 0:
+            image_width = get_image_width(submission)
+            resolution = round(image_width / 8.5)  # convert pixels to DPI
+            if image_width == 0:
                 print(
-                    f"{timestamp} INVALID {submission.author} {submission.link_flair_text}"
+                    f"{timestamp} 'width=' not found in selftext {submission.author} {submission.link_flair_text}"
                 )
-            resolution = round(width / 8.5)  # convert pixels to DPI
-            if width < MIN_WIDTH:
+                submission.report(f"POST IS MISSING IMAGE")
+                num_reports += 1
+            elif image_width < MIN_IMAGE_WIDTH:
                 print(
-                    f"{timestamp} FAIL {resolution}DPI {submission.author} {submission.link_flair_text}"
+                    f"{timestamp} REJECT {resolution}DPI {submission.author} {submission.link_flair_text}"
                 )
+                num_reports += 1
                 submission.report(f"LOW QUALITY IMAGE DETECTED: {resolution}DPI")
             else:
                 print(
@@ -91,8 +83,10 @@ def process_submissions(reddit):
                 )
 
 
-def get_image_width(submission):
+def get_image_width(submission) -> int:
     s = submission.selftext  # image width is embedded into image URL '...width=5100...'
+    if s == "":
+        return 0
     keyword = "width="
     try:
         start = s.index(keyword)
@@ -104,6 +98,12 @@ def get_image_width(submission):
         return int(s[start : (end - 1)])
     else:
         return int(s[start:end])
+
+
+def main():
+    reddit = instantiate_reddit()
+    process_submissions(reddit)
+    print(f"number of submissions reported: {num_reports}")
 
 
 if __name__ == "__main__":
